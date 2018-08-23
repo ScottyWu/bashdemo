870d247333840ae7fe383f9c5c899034904e2acd
Scott <helloseven306@gmail.com>
2018-08-24 02:10:09 +0800

    修改demo.js

--- demo.js
+++ demo.js
@@ -1,23 +1,22 @@
 #!/usr/bin/env node
-
 var fs = require('fs')
-var dirName = process.argv[2] // 你传的参数是从第2个开始的
+var dirName = process.argv[2] 
 
-fs.stat('dirName', function(err, stat){
+fs.stat('dirName', function(err, stat) 
+{
+    if(stat&&stat.isFile()) 
+        {console.log('文件存在');}
+    else 
+        {
+	    fs.mkdirSync("./" + dirName)  
+        process.chdir("./" + dirName) 
+        fs.mkdirSync('css') 
+        fs.mkdirSync('js') 
 
-    if(stat&&stat.isFile()) {
-	    console.log('文件存在');
-    }
-    else {
-	    fs.mkdirSync("./" + dirName)  // mkdir $1
-        process.chdir("./" + dirName) // cd $1
-        fs.mkdirSync('css') // mkdir css
-        fs.mkdirSync('js') // mkdir js
-
-        fs.writeFileSync("./index.html", "")
-        fs.writeFileSync("css/style.css", "") // ./ 可以不写，已经在当前目录
-        fs.writeFileSync("./js/main.js", "")
-
+        fs.writeFileSync("./index.html", "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>")
+        fs.writeFileSync("css/style.css", "h1{color: red;}") 
+        fs.writeFileSync("./js/main.js", "var string ='Hello World'\nalert(string)")
+        console.log('success')
         process.exit(0);
-    }
+        }
 })
--- demo.sh
+++ demo.sh
@@ -1,22 +1,13 @@
 if [ -d $1 ]; then
-   echo 'error: dir exists'
-   exit 1
+  echo 'error: dir exists'
+  exit
 else
-   mkdir $1
-   cd $1
-
-   echo "<!DOCTYPE>
-   <title>Hello</title>
-   <h1>Hi</h1>" > index.html
-
-   mkdir css
-   mkdir js
-
-   echo "h1{color: red;}" > css/style.css
-
-   echo "var string = 'Hello World'
-   alert(string)" > js/main.js
-
-   echo 'success'
-   exit 0
+  mkdir $1
+  cd $1
+  mkdir css js
+  echo -e '<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>' > index.html
+  echo -e 'h1{color: red;}' > css/style.css
+  echo -e 'var string = "Hello World"\nalert(string)' > js/main.js
+  echo 'success'
+  exit
 fi
